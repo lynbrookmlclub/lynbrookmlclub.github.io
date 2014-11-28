e763d6a5fd316de0d4a49ceabe96f139b20c34b9
matthewsotoudeh <matthewsot@outlook.com>
2014-11-27 21:29:00 -0800

    made menu button stay in the top left when you open nav
    attempted fix for issue #1

--- content/scripts/nav.js
+++ content/scripts/nav.js
@@ -8,10 +8,8 @@ function toggleNav() {
     if (nav.className != "nav-opened") {
         nav.className = "nav-opened";
         content.className = "nav-opened";
-        document.body.style.overflowY = "hidden";
     } else {
         nav.className = "";
         content.className = "";
-        document.body.style.overflowY = "scroll";
     }
 }
