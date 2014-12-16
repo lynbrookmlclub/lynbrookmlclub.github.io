a346677201de2f4c8b3402abdf50406a9f261cce
kennyyuan98 <kenny.k.yuan@gmail.com>
2014-12-15 23:44:23 -0800

    put in some update posts
    moving towards the removal of filler text. real updates were inputted.
    mr fulk's bio was taken out for now, until we get a response from him.

--- content/scripts/updates.js
+++ content/scripts/updates.js
@@ -1,13 +1,18 @@
 var updates = [
     {
-        "title":"New Website 1.0",
-        "date": "December 8th, 2014",
-        "content": "She gone shake it. LIke a red nose, like a, like a, like a red nose. Shake it for the dojo and the sensai. I almost got her a house up off King's way. I told her shake it like a red nose pitbull, And I'ma keep throwing. She gone shake it. LIke a red nose, like a, like a red nose. Shake it for the dojo like the sensai. I almost got her a house off King's way. I told her shake it like a red nose pitbull. And I'ma keep throwing She gone shake it. Like a red nose, like a, like a, like a red nose. Shake it for the"
+        "title":"Passed by Leg Council!",
+        "date": "November 4th, 2014",
+        "content": "Aw yeah. Today, our club proposal was passed by the legislative council. Lynbrook Web Dev is officially in business (as soon as next semester arrives)! Thank you to the 70+ people who filled out our interest form earlier in the month. Together, you played a huge role in getting this club started, and we are so excited to get all of you started on some code. Organization is currently underway and more information and details will be posted soon. Do not fear. All shall soon become clear."
     },
     {
-        "title":"New Website 2.0",
+        "title":"Updated Website",
         "date": "December 8th, 2014",
-        "content": "She gone shake it. LIke a red nose, like a, like a, like a red nose. Shake it for the dojo and the sensai. I almost got her a house up off King's way. I told her shake it like a red nose pitbull, And I'ma keep throwing. She gone shake it. LIke a red nose, like a, like a red nose. Shake it for the dojo like the sensai. I almost got her a house off King's way. I told her shake it like a red nose pitbull. And I'ma keep throwing She gone shake it. Like a red nose, like a, like a, like a red nose. Shake it for the She gone shake it. LIke a red nose, like a, like a, like a red nose. Shake it for the dojo and the sensai. I almost got her a house up off King's way. I told her shake it like a red nose pitbull, And I'ma keep throwing. She gone shake it. LIke a red nose, like a, like a red nose. Shake it for the dojo like the sensai. I almost got her a house off King's way. I told her shake it like a red nose pitbull. And I'ma keep throwing She gone shake it. Like a red nose, like a, like a, like a red nose. Shake it for the"
+        "content": "'She gone shake it. Like a red nose, like a, like a, like a red nose...' The original one-page Lynbrok Web Dev website has been updated into the full-feature, responsive site, which includes a calendar of (tentative) meeting times, this updates page, officer biographies, and a link to the soon-to-be-filled Github Repo of lesson presentations and resources. Lesson planning is underway, and - as soon as the website is debugged and finalized - a massive club launch will utterly shatter Facebook. Nah. Just be on the look out for a few posts about the start of the club."
+    },
+    {
+        "title":"First Meeting",
+        "date": "December 18th, 2014",
+        "content": "AT LAST, the time has come for this club to commence. The first Lynbrook Web Dev meeting will take place on Tuesday, January 13 at lunch in room 608. This first meeting will be quick and purely informational, so no need to bring anything. We'll be giving an overview of our plans for the semester as well as instructions for each of you as members. After we get this informational meeting out of the way, we will begin fun stuff the following week. For now, make sure to come out to our first meeting, as well as fill out this <a href='https://docs.google.com/forms/d/1H7QK5ZxHLnaRrGMjt-ONz0kxNNkysawqRO5etGMedqo/viewform?c=0&w=1' target='_blank'>membership form</a>"
     }
 ];
 
