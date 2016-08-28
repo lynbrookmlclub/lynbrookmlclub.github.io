f8c555232c8d2d912615a1878ecfe31fe91fd9ae
matthewsotoudeh <matthewsot@outlook.com>
2016-08-27 18:02:32 -0700

    Updated calendar source

--- content/scripts/calendar.js
+++ content/scripts/calendar.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('#calendar').fullCalendar({
     googleCalendarApiKey: 'AIzaSyDTGJh5n6dMIC5sREBfk8DVMRhXW4xt2cY',
     events: {
-      googleCalendarId: '6n6cg8961ilfi0g8c7mhdi17qc@group.calendar.google.com'
+      googleCalendarId: ' tc87q7o58cjcirq5edlhg8p8i8@group.calendar.google.com'
     },
     eventColor: '#E74B3C'
   });
