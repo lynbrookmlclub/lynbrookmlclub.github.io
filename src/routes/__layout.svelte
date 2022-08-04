08a93ca3879cb2816e2ff4cdeb219b0b275eb252
anish-lkakapragada <anish.lakkapragada@gmail.com>
2022-08-04 15:07:20 -0700

    plz work

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,16 @@
 <script>
   import "papercss/dist/paper.css";
   import Navbar from "spaper/components/Navbar.svelte";
+  import {base} from "$app/paths";
 </script>
 
 <Navbar>
   <h3 slot="brand">
-    <a href={`/`}>Lynbrook ML</a>
+    <a href={`${base}/`}>Lynbrook ML</a>
   </h3>
   <ul class="inline">
-    <li><a class="other-links" href={`/officers`}>About Us</a></li>
-    <li><a class="other-links" href={`/signup`}>Sign Up</a></li>
+    <li><a class="other-links" href={`${base}/officers`}>About Us</a></li>
+    <li><a class="other-links" href={`${base}/signup`}>Sign Up</a></li>
   </ul>
 </Navbar>
 <slot />
