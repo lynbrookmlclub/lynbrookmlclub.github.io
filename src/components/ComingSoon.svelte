d433b2d757036f4ef36d3b17400878f9f0a5d351
SneK152 <kakanisnehil@gmail.com>
2022-08-20 15:18:01 -0700

    adding the iframe stuff

--- src/components/ComingSoon.svelte
+++ src/components/ComingSoon.svelte
@@ -1,13 +1,33 @@
 <script lang="ts">
-  export let haveReturnButton : boolean; 
+  export let haveReturnButton: boolean;
   import { base } from "$app/paths";
   import Button from "spaper/components/Button.svelte";
 </script>
 
-<h1 class="header">Coming Soon! </h1>
+<h1 class="header">Coming Soon!</h1>
 {#if haveReturnButton}
   <div>
     <Button type="secondary" block href={`${base}/`}>Back to Home</Button>
+  </div>
+{:else}
+  <h2 class="header">For more content watch these videos:</h2>
+  <div style="gap: 1rem;">
+    <iframe
+      src="https://www.youtube.com/embed/Wbid5rvCGos"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      class="vid-iframe"
+    />
+    <iframe
+      src="https://www.youtube.com/embed/Xw-zxQSEzqo"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      class="vid-iframe"
+    />
   </div>
 {/if}
 
@@ -20,7 +40,14 @@
   }
 
   div {
+    display: flex;
+    justify-content: center;
     text-align: center;
     padding: 0 2rem;
+  }
+
+  .vid-iframe {
+    height: 315px;
+    width: 500px;
   }
 </style>
